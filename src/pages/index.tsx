af84570b3c0507d520ea5a75e769e80643e9928d
felipe <contatofelipecsr@gmail.com>
2021-03-16 19:10:31 -0300

    projeto finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ export default function Home(props: HomeProps) {
     <ChallengesProvider level={props.level} currentExperience={props.currentExperience} challengesCompleted={props.challengesCompleted}>
       <div className={styles.container}>
         <Head>
-          <title>Inicio. | move.it</title>
+          <title>Inicio | move.it</title>
         </Head>
 
         <ExperienceBar />
